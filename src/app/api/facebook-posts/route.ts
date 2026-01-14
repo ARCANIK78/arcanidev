a59534a4c2a209ff612c886b3fffd314ce077b9a
ARCANIK78 <loisgordisrour@gmail.com>
2026-01-14 14:05:12 -0400

    cambio de tokens del facebook de desarrollo.

--- src/app/api/facebook-posts/route.ts
+++ src/app/api/facebook-posts/route.ts
@@ -4,9 +4,9 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   const PAGE_ID = "768559203002227";
   const ACCESS_TOKEN =
-    "EAFXsKZCXyvHcBPBnDVPFDECGztDfkwYj8PkGzUqxZBUFqZAPMsY90xGtGrjI9XX638LcgP4WTNGoN2q6c4fKHtdS7Pa74DGs2L4I68ZAOcH5aZC128UplJxZAOKkmpZAwJuV62KMddqJUanlIeQaZB8ZAuGPEiiA0iD20z4apyx98vCQu8JFeb2wzak5WAZBlZA8a3d4kmZC";
+    "EAFXsKZCXyvHcBQcZC0WWJSOZBiMmL8SXRDnNKHhZBAlMkzOZA2OlrP3cpeTYAEXieSpasCk1m7ZBsmDvt6fEjHEGRCJNzO2LyawyalBjZCUiBFOz8bUzZBAky0ZAbSgoXcWmMaRZAUdUdkV3JYUpj56Oag1D1Re9F80kYwg1WDldRvryf9JY5QNo0Nt8udpYxP7HP1XseIW5iyDsgg0ikBTP1ZAkM5VynTAkUthRXbm3cgZD";
 
-const url = `https://graph.facebook.com/v19.0/${PAGE_ID}/posts?fields=id,message,created_time,full_picture,permalink_url,attachments{media,type}&access_token=${ACCESS_TOKEN}`;
+const url = `https://graph.facebook.com/v24.0/${PAGE_ID}/posts?fields=id,message,created_time,full_picture,permalink_url,attachments{media,type}&access_token=${ACCESS_TOKEN}`;
 
   try {
     const response = await fetch(url);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-// app/page.tsx
+
 "use client";
 import NextLink from 'next/link';
 import Paragraph from "@/components/paragraph";
